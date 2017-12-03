6874b3112ff04c9072f59208336cbbd2ffe44f0e
OCastelle <34087362+OCastelle@users.noreply.github.com>
2017-12-02 19:27:20 -0800

    Update index2.js

--- index2.js
+++ index2.js
@@ -88,40 +88,6 @@ bot.user.setPresence({ game: { name: "!+=soundblock=+!" , type: 0 } });
     }})
 }
 
-  if (msg.startsWith(prefix + 'WEATHER')) { // This checks to see if the beginning of the message is calling the weather command.
-    // You can find some of the code used here on the weather-js npm page in the description.
-
-    weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
-        if (err) message.channel.send(err);
-
-        // We also want them to know if a place they enter is invalid.
-        if (result === undefined || result.length === 0) {
-            message.channel.send('**Please enter a valid location.**') // This tells them in chat that the place they entered is invalid.
-            return; // This exits the code so the rest doesn't run.
-        }
-
-        // Variables
-        var current = result[0].current; // This is a variable for the current part of the JSON output
-        var location = result[0].location; // This is a variable for the location part of the JSON output
-
-        // Let's use an embed for this.
-        const embed = new Discord.RichEmbed()
-            .setDescription(`**${current.skytext}**`) // This is the text of what the sky looks like, remember you can find all of this on the weather-js npm page.
-            .setAuthor(`Weather for ${current.observationpoint}`) // This shows the current location of the weather.
-            .setThumbnail(current.imageUrl) // This sets the thumbnail of the embed
-            .setColor(0x85929e) // This sets the color of the embed, you can set this to anything if you look put a hex color picker, just make sure you put 0x infront of the hex
-            .addField('Timezone',`UTC${location.timezone}`, true) // This is the first field, it shows the timezone, and the true means `inline`, you can read more about this on the official discord.js documentation
-            .addField('Degree Type',location.degreetype, true)// This is the field that shows the degree type, and is inline
-            .addField('Temperature',`${current.temperature} Degrees`, true)
-            .addField('Feels Like', `${current.feelslike} Degrees`, true)
-            .addField('Winds',current.winddisplay, true)
-            .addField('Humidity', `${current.humidity}%`, true)
-
-            // Now, let's display it when called
-            message.channel.send({embed});
-    });
-}
-
   if (msg.includes('SHIT')) {
     message.delete();
     message.author.send('The WORD **shit** is banned, Do not attempt to type it again!')
